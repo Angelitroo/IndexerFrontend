191eb1ee4f6ba1d3bd1b99f1bb04e677f8d13bb3
AngelitroGX <150464424+Angelitroo@users.noreply.github.com>
2025-04-06 17:50:56 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IndexerFrontend',
+  webDir: 'www'
+};
+
+export default config;
