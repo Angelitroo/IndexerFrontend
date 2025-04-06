dd3579ef0b210786b18c83dd400605f8e3e593fc
AngelitroGX <150464424+Angelitroo@users.noreply.github.com>
2025-04-06 18:01:20 +0200

    Creacion archivos principales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,12 @@ export const routes: Routes = [
     redirectTo: 'login',
     pathMatch: 'full',
   },
+  {
+    path: "registro",
+    loadComponent: () => import('./registro/registro.component').then((m) => m.RegistroComponent),
+  },
+  {
+    path:"principal",
+    loadComponent: () => import('./principal/principal.component').then((m) => m.PrincipalComponent),
+  }
 ];
