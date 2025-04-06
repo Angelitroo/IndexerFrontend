18b7d004fc17fd662bf856979942ead5e406da45
AngelitroGX <150464424+Angelitroo@users.noreply.github.com>
2025-04-06 19:25:02 +0200

    Login vistoso, pensar imagen fondo imagen logo

---
+++ src/app/models/Producto.ts
@@ -0,0 +1,11 @@
+export interface Producto {
+  id: number;
+  tittle: string;
+  discount: string;
+  actualPrice: number;
+  oldPrice: number;
+  image: string;
+  rating: string;
+  delivery: string;
+  url: string;
+}
