957965b51f03091a80dbf53e6f732ca8a4d488ef
AngelitroGX <150464424+Angelitroo@users.noreply.github.com>
2025-05-13 14:25:30 +0200

    intentando hacer un scroll horizontal que funcione con las rueda del raton... pero si son solo 5 elementos por empresa no deberia de hacer falta.?

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -6,6 +6,7 @@ import { Producto } from '../models/Producto';
 import {addIcons} from "ionicons";
 import {notificationsOutline, personCircleOutline} from "ionicons/icons";
 import {MenuizquierdaComponent} from "../menuizquierda/menuizquierda.component";
+import { AfterViewInit, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
@@ -14,8 +15,31 @@ import {MenuizquierdaComponent} from "../menuizquierda/menuizquierda.component";
   templateUrl: './principal.component.html',
   styleUrls: ['./principal.component.scss']
 })
-export class PrincipalComponent {
+export class PrincipalComponent implements AfterViewInit{
   empresas: string[] = ['Amazon', 'eBay', 'AliExpress', 'PCComponentes', 'MediaMarkt', 'Carrefour', 'El Corte Inglés'];
+
+  @ViewChildren('scrollArea') scrollAreas!: QueryList<ElementRef<HTMLElement>>;
+
+  ngAfterViewInit() {
+    this.scrollAreas.forEach((areaRef) => {
+      const area = areaRef.nativeElement;
+
+      area.addEventListener(
+        'wheel',
+        (e: WheelEvent) => {
+          if (e.deltaY !== 0) {
+            e.preventDefault();
+            area.scrollLeft += e.deltaY;
+          }
+        },
+        { passive: false }
+      );
+    });
+  }
+
+
+
+
 
   productos: Producto[] = [
     {
@@ -50,6 +74,39 @@ export class PrincipalComponent {
       rating: '4.6',
       delivery: 'Entrega en 48h',
       url: 'https://example.com/smartwatch'
+    },
+    {
+      id: 4,
+      title: 'Cámara Deportiva 4K',
+      discount: '25%',
+      actualPrice: 74.99,
+      oldPrice: 99.99,
+      image: 'https://correos-marketplace.ams3.cdn.digitaloceanspaces.com/prod-new/uploads/correos-marketplace-shop/1/product/108041-31wkrigo-klack-camara-deportiva-de-accion-klack-full-hd-1080p-resistente-al-agua-negro-1.jpg',
+      rating: '4.7',
+      delivery: 'Entrega en 24h',
+      url: 'https://example.com/camara-deportiva'
+    },
+    {
+      id: 5,
+      title: 'Altavoz Bluetooth Portátil',
+      discount: '30%',
+      actualPrice: 34.99,
+      oldPrice: 49.99,
+      image: 'https://vieta.es/cdn/shop/files/8431543119867_01_030853ba-ca86-469b-8085-e29b77c7b141.jpg?v=1743670682',
+      rating: '4.4',
+      delivery: 'Entrega rápida',
+      url: 'https://example.com/altavoz-bluetooth'
+    },
+    {
+      id: 6,
+      title: 'Monitor Full HD 24"',
+      discount: '20%',
+      actualPrice: 119.99,
+      oldPrice: 149.99,
+      image: 'https://m.media-amazon.com/images/I/81QpkIctqPL._AC_SL1500_.jpg',
+      rating: '4.6',
+      delivery: 'Entrega en 48h',
+      url: 'https://example.com/monitor-fullhd'
     }
   ];
   constructor() {
